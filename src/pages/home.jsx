459c0c112ff699e98ac3cd583e195868fbb52c8b
Andrei <zoitber9@mail.ru>
2023-08-05 21:53:13 +0300

    feat: Add slider

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Veggie from "../components/veggie";
+import Popular from "../components/popular";
+
+const Home = () => {
+    return (
+        <div>
+            <Veggie/>
+            <Popular/>
+        </div>
+    );
+};
+
+export default Home;
