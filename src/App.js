1af693ee437556151b2230bd89865adf10d0206a
Andrei <zoitber9@mail.ru>
2023-08-06 23:33:05 +0300

    feat: Add SearchBar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Pages from "./pages/pages";
 import Category from "./components/category";
 import {BrowserRouter} from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                <Search/>
                 <Category/>
                 <Pages/>
             </div>
--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Home from "./home";
 import {Route, Routes} from 'react-router-dom'
 import Cuisine from "./Cuisine";
+import Searched from "./Searched";
+import Recipe from "./recipe";
 
 const Pages = () => {
     return (
-            <Routes>
-                <Route path={'/'} element={<Home/>}/>
-                <Route path={'/cuisine/:type'} element={<Cuisine/>}/>
-            </Routes>
+        <Routes>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={'/cuisine/:type'} element={<Cuisine/>}/>
+            <Route path={'/searched/:search'} element={<Searched/>}/>
+            <Route path={'/recipe/:name'} element={<Recipe/>}/>
+        </Routes>
     );
 };
 
