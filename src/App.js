214bdace572381ef1a24f951d44d1c4c063057a3
Andrei <zoitber9@mail.ru>
2023-08-23 12:43:48 +0300

    feat: Add info about recipes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import './App.css';
 import Pages from "./pages/pages";
 import Category from "./components/category";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Link} from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import {GiKnifeFork} from "react-icons/gi";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
+        <div className="App">
+            <BrowserRouter>
+                <Nav>
+                    <GiKnifeFork/>
+                    <Logo to={'/'}>Delicious</Logo>
+                </Nav>
                 <Search/>
                 <Category/>
                 <Pages/>
-            </div>
-        </BrowserRouter>
+            </BrowserRouter>
+        </div>
     );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-weight: 400;
+  font-size: 1.5rem;
+  font-family: 'Lobster Two', cursive;
+`
+
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`
 
 export default App;
 
--- src/pages/recipe.jsx
+++ src/pages/recipe.jsx
@@ -25,14 +25,28 @@ const Recipe = () => {
             </div>
             <Info>
                 <Button className={activeTab === 'instructions' ? 'active' : ''}
-                        onClick={() => {setActiveTab('instructions')
-                }}>Instructions</Button>
+                        onClick={() => {
+                            setActiveTab('instructions')
+                        }}>Instructions</Button>
                 <Button className={activeTab === 'ingredients' ? 'active' : ''}
-                        onClick={() => {setActiveTab('ingredients')
-                }}>Ingredients</Button>
-                <div>
-                    <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
-                </div>
+                        onClick={() => {
+                            setActiveTab('ingredients')
+                        }}>Ingredients</Button>
+                {activeTab === 'instructions' && (
+                    <div>
+                        <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+                        <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
+                    </div>
+                )}
+                {activeTab === 'ingredients' && (
+                    <ul>
+                        {details.extendedIngredients.map((ingredient) => (
+                            <li key={ingredient.id}>{ingredient.original}</li>
+                        ))}
+                    </ul>
+                )}
+
+
             </Info>
         </DetailWrapper>
     );
